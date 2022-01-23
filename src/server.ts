48d0814e959214e4b530043d0e70b17b8794e8a7
xd-elon <alandevelope@gmail.com>
2022-01-23 01:18:50 -0300

    eslint and prettier add

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response
-})
+app.get("/", (reuqest, response) => {
+  return response.json({ message: "hello world" });
+});
 
-app.listen(4545)
+app.listen(4545, () => console.log("sever is running"));
