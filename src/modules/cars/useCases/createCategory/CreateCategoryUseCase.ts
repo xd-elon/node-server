509b4ae80e159d9ccfea68fa57ea6efd73a98beb
xd-elon <alandevelope@gmail.com>
2022-03-05 23:26:10 -0300

    controller

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,11 +1,11 @@
-import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-class CreateCategoryService {
+export class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
   execute({ name, description }: IRequest): void {
@@ -18,5 +18,3 @@ class CreateCategoryService {
     this.categoriesRepository.create({ name, description });
   }
 }
-
-export { CreateCategoryService };
