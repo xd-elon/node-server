46dd55a0433b2a6f166594a9123693bdb19886d9
xd-elon <alandevelope@gmail.com>
2022-03-10 16:51:12 -0300

    swagger

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,9 +1,54 @@
+import { parse as csvParse } from "csv-parse";
 import fs from "fs";
 
-export class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createWriteStream(file.path);
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
-    stream.pipe();
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+export class ImportCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+
+      const parseFile = csvParse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    console.log(categories);
+
+    categories.map(async (category) => {
+      const { name, description } = category;
+
+      const existCategory = this.categoriesRepository.findByName(name);
+
+      if (!existCategory) {
+        this.categoriesRepository.create({ name, description });
+      }
+    });
   }
 }
--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,9 @@
+import { CategoriesRepository } from "../../repositories/implementetions/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const importCategoryUseCase = new ImportCategoryUseCase();
+const categoriesRepository = CategoriesRepository.getIntance();
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 export const importCategoryController = new ImportCategoryController(
   importCategoryUseCase
 );
