5c6f15b6c26d487792eaa09d26cac2879a8b2c9f
xd-elon <alandevelope@gmail.com>
2022-03-15 15:34:20 -0300

    migrations add

---
+++ src/database/migrations/1647369223287-CreateCategories.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateCategories1647369223287 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
