a55af02250db5112b34bc09dd65089554581a70f
xd-elon <alandevelope@gmail.com>
2022-03-22 22:12:28 -0300

    creadte alteruserDeleteUsername

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -4,8 +4,9 @@ import { container } from "tsyringe";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 export class CreateUserController {
-  async handle(request: Request, response: Response) {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { name, username, email, password, driver_license } = request.body;
+
     const createUserUseCase = container.resolve(CreateUserUseCase);
 
     await createUserUseCase.execute({
