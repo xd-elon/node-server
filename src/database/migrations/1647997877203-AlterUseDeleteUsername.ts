46f18cdabfd83ba09833a25ebaf813d2ebeb4d01
xd-elon <alandevelope@gmail.com>
2022-03-25 16:01:09 -0300

    verifi user, cript password, add JWT

--- src/database/migrations/1647997877203-AlterUseDeleteUsername.ts
+++ src/database/migrations/1647997877203-AlterUseDeleteUsername.ts
@@ -1,11 +1,17 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
 export class AlterUseDeleteUsername1647997877203 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("users", "username");
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "users",
+      new TableColumn({
+        name: "username",
+        type: "varchar",
+      })
+    );
+  }
 }
--- src/modules/accounts/repositories/implementations/UserRepository.ts
+++ src/modules/accounts/repositories/implementations/UserRepository.ts
@@ -13,19 +13,23 @@ export class UserRepository implements IUsersRepository {
 
   async create({
     name,
-    username,
     email,
     driver_license,
     password,
   }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
-      username,
       email,
       driver_license,
       password,
     });
 
     await this.repository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
+
+    return user;
   }
 }
