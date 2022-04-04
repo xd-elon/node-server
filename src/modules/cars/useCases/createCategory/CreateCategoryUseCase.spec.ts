440b5109b61df290857f5f8a776e8b6260d66436
xd-elon <alandevelope@gmail.com>
2022-04-03 23:35:39 -0300

    add jest and create test

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,9 @@
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+describe("Criar category", () => {
+  it("should be able to create a new category", () => {
+    const createCategory = new CreateCategoryUseCase();
+
+    expect().toBe();
+  });
+});
