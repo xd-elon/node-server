e6a70dcb56fdb19919e3351184130f18d87fb2fc
xd-elon <alandevelope@gmail.com>
2022-04-04 23:54:13 -0300

    in-memory and CreateCategoryUse case test

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,9 +1,28 @@
+import { CategoryRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-describe("Criar category", () => {
-  it("should be able to create a new category", () => {
-    const createCategory = new CreateCategoryUseCase();
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoryRepositoryInMemory: CategoryRepositoryInMemory;
 
-    expect().toBe();
+describe("Criar category", () => {
+  beforeAll(() => {
+    categoryRepositoryInMemory = new CategoryRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoryRepositoryInMemory
+    );
+  });
+
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "category test",
+      description: "info created for test",
+    };
+
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    categoryRepositoryInMemory.findByName(category.name);
   });
 });
