fd02d8785d9a58e9f07b6d5fc0b0833e2b8ce8fa
xd-elon <alandevelope@gmail.com>
2022-04-18 13:32:04 -0300

    admin middleware

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,16 +1,11 @@
-import { createConnection, getConnectionOptions } from "typeorm";
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-interface IOptions {
-  host: string;
-}
+export default async (host = "database"): Promise<Connection> => {
+  const defaultOtions = await getConnectionOptions();
 
-getConnectionOptions().then((options) => {
-  const newOptions = options as IOptions;
-  newOptions.host = "database"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-  if (newOptions.host === "database") {
-    console.log("connection database is equal");
-  }
-  createConnection({
-    ...options,
-  });
-});
+  return createConnection(
+    Object.assign(defaultOtions, {
+      host,
+    })
+  );
+};
