6b588d4f7667864d90e3325d0eaaf9fb51cd8ee7
xd-elon <alandevelope@gmail.com>
2022-05-08 13:30:59 -0300

    cars_image

--- src/shared/infra/typeorm/migrations/1651875914981-CreateCarImagens.ts
+++ src/shared/infra/typeorm/migrations/1651875914981-CreateCarImagens.ts
@@ -1,11 +1,31 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateCarImagens1651875914981 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: "cars_image",
+        columns: [
+          { name: "id", type: "uuid", isPrimary: true },
+          {
+            name: "car_id",
+            type: "uuid",
+          },
+          {
+            name: "image_name",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("cars_image");
+  }
 }
